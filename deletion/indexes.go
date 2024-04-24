25d3dfe9a86dc4251bdad02d35cf41cf90f8efd9
odhiambo_athooh <s.athooh@gmail.com>
2024-04-24 23:40:04 +0300

    delete package updates

--- deletion/indexes.go
+++ deletion/indexes.go
@@ -1,23 +1,32 @@
 package delete
 
 import (
-	"fmt"
+	"fmt" // Importing the fmt package for printing messages
 )
 
+// RemoveAtIndex removes an element at the specified index from a slice of strings.
+// It returns the modified slice after removal.
 func RemoveAtIndex(s []string, i int) []string {
+	// Checking if the index is out of range
 	if i <= 0 || i > len(s) {
-		fmt.Println("Index Out of range.")
+		fmt.Println("Index Out of range.") // Printing an error message if the index is invalid
 	} else {
+		// Removing the element at index i from the slice
 		s = append(s[:i], s[i+1:]...)
 	}
-	return s
+	return s // Returning the modified slice
 }
 
+// RemoveAtIndex2 removes an element at the specified index from a slice of strings
+// and the next element after that index.
+// It returns the modified slice after removal.
 func RemoveAtIndex2(s []string, i int) []string {
+	// Checking if the index is out of range
 	if i <= 0 || i > len(s) {
-		fmt.Println("Index Out of range.")
+		fmt.Println("Index Out of range.") // Printing an error message if the index is invalid
 	} else {
+		// Removing the element at index i and the next element from the slice
 		s = append(s[:i], s[i+2:]...)
 	}
-	return s
+	return s // Returning the modified slice
 }
